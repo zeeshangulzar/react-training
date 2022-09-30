25bd053840f6df678ded65d67eb45efab91a9bb1
Zeeshan Gulzar <xeeshangulzar@gmail.com>
2022-09-30 15:31:25 +0500

    Created an intinal state with typescript redux

---
+++ frontent/src/features/posts/PostSlice.tsx
@@ -0,0 +1,37 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import produce from "immer";
+import { RootState } from "../../app/store"
+
+export enum Statuses {
+    Initial = "Not Fetched",
+    Loading = "Loading...",
+    UpToDate = "UpTo Date",
+    Deleted = "Deleted",
+    Error = "Error"
+}
+
+export interface PostState {
+    id?: number;
+    title?: string;
+    body?: string;
+    created_at?: any;
+    updated_at?: any;
+}
+
+export interface PostsState {
+    posts: PostState[];
+    status: string;
+}
+
+const initialState: PostsState = {
+    posts: [
+        {
+            id: 0,
+            title: "",
+            body: "",
+            created_at: "",
+            updated_at: "",
+        }
+    ],
+    status: Statuses.Initial
+}
